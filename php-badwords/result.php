3d87bc52fbe91eed0fb48974b6fd8d7ecbdac173
masepe95 <marcosepe10@gmail.com>
2023-07-14 12:57:39 +0200

    censure works

---
+++ php-badwords/result.php
@@ -0,0 +1,24 @@
+<?php
+$censored_paragraph = str_replace(strtolower($_POST["bad-word"]), "***", strtolower($_POST['paragraph']));
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+
+<body>
+    <h1> Parola da censusare: </h1>
+    <h2> <?php echo $_POST["bad-word"]; ?> </h2>
+    <h1> Testo NON censurato: </h1>
+    <h2> <?php echo $_POST['paragraph']; ?> </h2>
+    <h1> Testo CENSURATO: </h1>
+    <h3> <?= $censored_paragraph ?></h3>
+</body>
+
+</html>
